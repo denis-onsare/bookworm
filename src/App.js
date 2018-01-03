5a97a6df1c5b273bf1c2652b291658f0abf06742
denis-onsare <donnsare@gmail.com>
2018-01-04 02:05:59 +0300

    Setup home and login pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from "./components/pages/HomePage";
+import Login from "./components/pages/Login"
 
-const App = () => <div>Hello world from react</div>;
+const App = () => (
+<div>
+  <Route path="/" exact component={ HomePage } />
+  <Route path="/login" exact component={ Login } />
+ 
+</div>
+);
 
 export default App;
---
+++ src/components/pages/Login.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Login = ({}) => (
+    <div>
+        <h1>Login</h1>
+    </div>
+);
+
+Login.propTypes = {};
+
+export default Login;
